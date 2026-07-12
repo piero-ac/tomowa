d0a424576952eb2b399f9afeb8a8c0f5e06816ee
Piero Coronado Flores <pacoronado01@gmail.com>
2026-07-12 15:46:57 -0400

    chore: add error and not found middleware

---
+++ apps/api/src/middleware/not-found.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+export function notFoundHandler(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	res.status(404).json({
+		message: "Route not found",
+	});
+}
