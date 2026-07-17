bb0f3649199c763ff7860d68a7e83ecdba127202
Piero Coronado Flores <pacoronado01@gmail.com>
2026-07-17 13:58:07 -0400

    feat: set JSON request body size limit to 100kb

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -13,7 +13,7 @@ export const app = express();
 
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "100kb" }));
 
 app.use("/health", healthRouter);
 app.use("/sessions", sessionsRouter);
