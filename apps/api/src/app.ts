0f1a55fed77a1fe250fa52cdd27afa5924669a46
Piero Coronado Flores <pacoronado01@gmail.com>
2026-07-17 13:52:42 -0400

    feat: add log error middleware

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -4,8 +4,10 @@ import helmet from "helmet";
 import healthRouter from "./routes/heath.routes.js";
 import sessionsRouter from "./routes/session.routes.js";
 import meRouter from "./routes/me.routes.js";
-import { notFoundHandler } from "./middleware/not-found.js";
+
 import { errorHandler } from "./middleware/error-handler.js";
+import { logErrors } from "./middleware/log-errors.js";
+import { notFoundHandler } from "./middleware/not-found.js";
 
 export const app = express();
 
@@ -18,4 +20,5 @@ app.use("/sessions", sessionsRouter);
 app.use("/me", meRouter);
 
 app.use(notFoundHandler);
+app.use(logErrors);
 app.use(errorHandler);
